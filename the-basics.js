2affdbefef3c645c4cfa3eaa34c77299adf2b78d
Hilary Goldberg <hilarygoldberg@MacBook-Pro-9.local>
2019-12-17 13:17:09 -0500

    i think this one works

--- the-basics.js
+++ the-basics.js
@@ -1,24 +1,3 @@
-var substringMatcher = function(strs) {
-  return function findMatches(q, cb) {
-    var matches, substringRegex;
-
-    // an array that will be populated with substring matches
-    matches = [];
-
-    // regex used to determine if a string contains the substring `q`
-    substrRegex = new RegExp(q, 'i');
-
-    // iterate through the pool of strings and for any string that
-    // contains the substring `q`, add it to the `matches` array
-    $.each(strs, function(i, str) {
-      if (substrRegex.test(str)) {
-        matches.push(str);
-      }
-    });
-
-    cb(matches);
-  };
-};
 
 var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
   'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
@@ -45,5 +24,5 @@ $('#the-basics .typeahead').typeahead({
 },
 {
   name: 'states',
-  source: substringMatcher(states)
+  source: states
 });
