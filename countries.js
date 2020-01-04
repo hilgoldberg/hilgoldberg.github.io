5260f7a606478bf5caaf55d9df50aa3b29743092
Hilary Goldberg <hilarygoldberg@MacBook-Pro-9.local>
2020-01-03 16:31:24 -0800

    adding countries

---
+++ countries.js
@@ -0,0 +1,20 @@
+
+var countries = ['Republic of Hil', 'Goldbergistan', 'Hil"s Hill'
+];
+
+var states = new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.whitespace,
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    local: countries
+});
+
+
+$('#prefetch .typeahead').typeahead({
+  hint: true,
+  highlight: true,
+  minLength: 1
+},
+{
+  name: 'countries',
+  source: countries
+});
